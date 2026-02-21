e2c084e3bf3cbf087c4f0fbd680ffeb0d5b6881d
ilzohf-dev <ilzo.hf@gmail.com>
2026-02-21 03:01:51 +0000

    finalizado cadastro de cartas

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -11,6 +11,44 @@ int main() {
 
   // Área para exibição dos dados da cidade
 
+  char estado;
+  char codigo[], nomedacidade[];
+  int populaçao, pontosturisticos;
+  float areakm, PIB;
+
+  printf ("Digite o Estado: \n");
+  scanf ("%c", &estado);
+
+  printf ("Digite o Código: \n");
+  scanf ("%s", &codigo);
+
+  printf ("Digite o Nome da Cidade: \n");
+  scanf ("%s", &nomedacidade);
+
+  printf ("Digite a População: \n");
+  scanf ("%d", &populaçao);
+
+  printf("Digite a Área: \n");
+  scanf ("%f", &areakm);
+
+  printf ("Digite o PIB: \n");
+  scanf ("%1f", &PIB);
+  
+  printf ("Digite o Número de Pontos Turísticos: \n");
+  scanf ("%d", &pontosturisticos);
+
+  printf (" Carta 1: \n");
+  Printf ("Estado: %c \n" estado);
+  printf ("Código: %s \n" codigo);
+  printf ("Nome da Cidade: %s \n" nomedacidade);
+  printf ("População: %d \n" populaçao);
+  printf ("Área: %f \n" areakm);
+  printf ("PIB: %1f \n" PIB);
+  printf ("Número de Pontos Turísticos: %d \n" pontosturisticos);
+
+  
+
+
   
 return 0;
 } 
