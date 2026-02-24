c1de9290180eecefbe1c3a6d75d61716d2001750
ilzohf-dev <ilzo.hf@gmail.com>
2026-02-24 02:52:27 +0000

    finalizando nivel 2

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -4,50 +4,64 @@
 // Tema 1 - Cadastro das cartas
 // Objetivo: No nível novato você deve criar as cartas representando as cidades utilizando scanf para entrada de dados e printf para exibir as informações.
 
-int main() {
-  // Área para definição das variáveis para armazenar as propriedades das cidades
+int main(){
 
-  // Área para entrada de dados
+    // Área para definição das variáveis para armazenar as propriedades das cidades
 
-  // Área para exibição dos dados da cidade
+    char estado;
+    char codigo[20];
+    char nomedacidade[20];
+    int população;
+    int pontosturisticos;
+    float areakm;
+    float pib;
+    float densidadepopulacional;
+    float pibpercapita;
 
-  char estado;
-  char codigo[20], nomedacidade[20];
-  int populaçao, pontosturisticos;
-  float areakm, PIB;
+    // Área para entrada de dados
 
-  printf ("Digite o Estado: \n");
-  scanf ("%c", &estado);
+    printf ("Digite o estado: \n");
+    scanf ("%c", &estado);
 
-  printf ("Digite o Código: \n");
-  scanf ("%s", &codigo);
+    printf ("Digite o Código: \n");
+    scanf ("%s", &codigo);
 
-  printf ("Digite o Nome da Cidade: \n");
-  scanf ("%s", &nomedacidade);
+    printf ("Digite o nome da cidade: \n");
+    scanf ("%s", &nomedacidade);
 
-  printf ("Digite a População: \n");
-  scanf ("%d", &populaçao);
+    printf ("Digite a população: \n");
+    scanf ("%d", &população);
 
-  printf("Digite a Área: \n");
-  scanf ("%f", &areakm);
+    printf ("Digite a Área: \n");
+    scanf ("%f", &areakm);
 
-  printf ("Digite o PIB: \n");
-  scanf ("%1f", &PIB);
-  
-  printf ("Digite o Número de Pontos Turísticos: \n");
-  scanf ("%d", &pontosturisticos);
+    printf ("Dikgite o PIB: \n");
+    scanf ("%f", &pib);
 
-  printf (" Carta 1: \n");
-  printf ("Estado: %c \n", estado);
-  printf ("Código: %s \n", codigo);
-  printf ("Nome da Cidade: %s \n", nomedacidade);
-  printf ("População: %d \n", populaçao);
-  printf ("Área: %f \n", areakm);
-  printf ("PIB: %1f \n", PIB);
-  printf ("Número de Pontos Turísticos: %d \n", pontosturisticos);
+    printf ("Digite o número de pontos turísticos: \n");
+    scanf ("%d", &pontosturisticos);
+
+    // Área para calculos de dados
+
+    densidadepopulacional = população / areakm;
+
+    pibpercapita = pib / população;
+    
+    // Área para exibição dos dados da cidade
+    printf ("\n");
+    printf ("Carta 1: \n");
+    printf ("Estado: %c \n", estado);
+    printf ("Código: %s \n", codigo);
+    printf ("Nome da Cidade: %s \n", nomedacidade);
+    printf ("População: %d \n", população);
+    printf ("Área: %F Km² \n",areakm);
+    printf ("PIB: %f \n", pib);
+    printf ("Número de Pontos Turísticos: %d \n", pontosturisticos);
+    printf ("Densidade Populacional: %f hab/Km² \n", densidadepopulacional);
+    printf ("PIB per Capita: %f \n", pibpercapita);
+               
 
 
-
-  
-return 0;
-} 
+    return 0;
+    
+}
